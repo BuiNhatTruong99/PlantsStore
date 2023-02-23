43ca106f3c9ae2c6a3e516b09760e84e2274fcae
dohung02 <hungdpps19239@fpt.edu.vn>
2023-02-24 05:59:33 +0700

    rename categoryserviceImpl

--- Website-Ban-Cay-Canh/src/main/java/com/datamining/servide/impl/CategoryServiceImpl.java
+++ Website-Ban-Cay-Canh/src/main/java/com/datamining/servide/impl/CategoryServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class CaetgoryServiceImpl implements CategoryService {
+public class CategoryServiceImpl implements CategoryService {
     @Autowired
     CategoryDAO cdao;
 
