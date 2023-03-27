8a4236bc95a1f58570bf16a7e875ae2e0b67b254
dohung02 <hungdpps19239@fpt.edu.vn>
2023-03-27 22:08:29 +0700

    update interface admin

--- Website-Ban-Cay-Canh/src/main/java/com/datamining/entity/ProductSize.java
+++ Website-Ban-Cay-Canh/src/main/java/com/datamining/entity/ProductSize.java
@@ -10,6 +10,7 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.Data;
