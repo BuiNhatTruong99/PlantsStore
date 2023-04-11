611fcf5147dad8bbca428d3313951d387ec4b5a4
BuiNhatTruong99 <tbui0936@gmail.com>
2023-04-11 09:33:47 +0700

    Update AccountDAO.java

--- Website-Ban-Cay-Canh/src/main/java/com/datamining/dao/AccountDAO.java
+++ Website-Ban-Cay-Canh/src/main/java/com/datamining/dao/AccountDAO.java
@@ -3,6 +3,7 @@ package com.datamining.dao;
 import com.datamining.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,5 +13,7 @@ public interface AccountDAO extends JpaRepository<Account, Integer> {
     @Query("SELECT a FROM Account a WHERE a.username LIKE %?1%")
     List<Account> findByUsername(String username);
 
+    @Query("select u from Account u where u.username = :username ")
+    public Account findByTk(@Param("username") String username);
 
 }
